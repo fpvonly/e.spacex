0b088b9a476859117ab2f0120acb44eec45c00e1
Ari Petäjäjärvi <fpvonly@gmail.com>
2018-02-25 19:45:03 +0200

    CI deploy commands added

--- codeship_deploy.sh
+++ codeship_deploy.sh
@@ -1,7 +1,2 @@
-#setup
-nvm install 9.5.0
-npm install -g npm@latest
-cd e.spacex
-npm install
 # deploy
 ssh deploy@146.185.148.22 'git config --global user.email "fpvonly@gmail.com"; git config --global user.name "fpvonly@gmail.com"; cd e.spacex/; git checkout master; git pull; npm install; npm run prodbuild; git add ./build; git commit -m "Codeship CI build (production) --skip-ci"; git push; sudo npm run foreverrestart;'
